43bffc3ad9acdf960093afb579150d82c4addca4
Martin <mlienhard@indeed.com>
2023-11-03 08:12:46 -0700

    Initial Commit

---
+++ Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Martin Lienhard on 11/3/23.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
